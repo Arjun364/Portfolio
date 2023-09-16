0ca4df1cbc29883a6c5c249547e5390cbd39151b
Arjun AV <avarjun360@gmail.com>
2023-09-16 15:02:34 +0530

    Preloader
    Added a preloader to the website

--- scripts.js
+++ scripts.js
@@ -4,3 +4,19 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+// preloader setting--------------------------------------
+
+// Wait for the website to load
+window.addEventListener('load', () => {
+    // Remove the preloader
+    const preloader = document.querySelector('.preloader');
+    preloader.style.display = 'none';
+
+    // Show the content
+    const content = document.querySelector('.content');
+    content.style.display = 'block';
+
+    // Enable scrolling
+    document.body.style.overflow = 'auto';
+});
